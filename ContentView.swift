3a7bee65099ff7b7dd113f66e0ae2495079b2a66
wteen-alghamdy <wteenalghamdy1@gmail.com>
2026-02-02 17:38:41 +0300

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Walker's-Gate
+//
+//  Created by Wteen Alghamdy on 13/08/1447 AH.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            // استدعاء الخريطة لتملأ الشاشة
+            MapViewRepresentable()
+                .edgesIgnoringSafeArea(.all)
+            
+            // واجهة علوية بسيطة لاسم اللعبة
+            VStack {
+                HStack {
+                    Text("Walker's Gate")
+                        .font(.system(size: 24, weight: .bold, design: .monospaced))
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.black.opacity(0.6))
+                        .cornerRadius(12)
+                    Spacer()
+                }
+                .padding(.top, 50)
+                .padding(.leading, 20)
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Walker_s_GateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Walker_s_GateApp.swift
+//  Walker's-Gate
+//
+//  Created by Wteen Alghamdy on 13/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Walker_s_GateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
